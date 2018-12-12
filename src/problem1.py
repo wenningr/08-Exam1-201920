3032db15cf887169d1349f33ade192beb9ae239f
wenningr <wenningr@rose-hulman.edu>
2018-12-11 20:45:55 -0500

    DONE

--- src/problem1.py
+++ src/problem1.py
@@ -3,13 +3,13 @@ Exam 1, problem 1.
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and Greg Wenning.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 # -----------------------------------------------------------------------------
-# TODO: 2. Right-click on the  src  folder and
+# DONE: 2. Right-click on the  src  folder and
 #              Mark Directory as ... Sources Root,
 #          if you have not already done so.
 # -----------------------------------------------------------------------------
@@ -101,11 +101,26 @@ def problem1(square, thickness, window):
       :type window:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
+    # DONE: 3. Implement and test this function.  SEE THE PICTURES in the PDF!
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    square.attach_to(window)
+    square.outline_color = square.outline_color
 
+    point = rg.Point(square.center.x, square.center.y+ square.length_of_each_side)
 
+    circle = rg.Circle(point, square.length_of_each_side/2)
+    circle.fill_color = square.fill_color
+    circle.attach_to(window)
+    circle.outline_color = 'black'
+    circle.outline_thickness = thickness
+
+    point2 = rg.Point(square.center.x- square.length_of_each_side/2, square.center.y)
+    line = rg.Line(point2, circle.center)
+    line.color = square.outline_color
+    line.thickness = circle.outline_thickness
+    line.attach_to(window)
+    window.render()
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
